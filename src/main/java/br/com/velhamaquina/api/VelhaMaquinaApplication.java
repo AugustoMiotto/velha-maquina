59400c848a84c0310057f2a29a51c6e423d3e956
Augusto Miotto <augustommiotto@gmail.com>
2025-10-09 14:45:55 -0300

    Initial commit: Configuração do projeto Spring Boot

---
+++ src/main/java/br/com/velhamaquina/api/VelhaMaquinaApplication.java
@@ -0,0 +1,13 @@
+package br.com.velhamaquina.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelhaMaquinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelhaMaquinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/velhamaquina/api/VelhaMaquinaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.velhamaquina.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelhaMaquinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
